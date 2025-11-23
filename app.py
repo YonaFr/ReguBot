f59a85aa5e9e589a87cf5a8873dfaebdcb345de5
YonaFr <138700294+YonaFr@users.noreply.github.com>
2025-11-23 11:10:53 +0700

    -

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def get_pdf_text(pdf_docs):
 
 
 def get_text_chunks(text):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
     chunks = splitter.split_text(text)
     return chunks
 
